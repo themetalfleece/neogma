5526658fbc11c56e938d9cc9be9a495734efc3c1
Jason Athanasoglou <jathanasoglou@outlook.com>
2019-07-16 22:51:24 +0300

    use .env for Driver.connect

--- src/Driver.ts
+++ src/Driver.ts
@@ -1,15 +1,24 @@
 import * as neo4j_driver from 'neo4j-driver';
+import * as dotenv from "dotenv";
 const neo4j = neo4j_driver.v1;
 
 let driver: neo4j_driver.v1.Driver;
+
+dotenv.config();
 
 export const getDriver = () => {
     return driver;
 }
 
-export const connect = (url, username, password) => {
-    driver = neo4j.driver(
-        url,
-        neo4j.auth.basic(username, password),
-    );
+export const connect = () => {
+    try {
+        driver = neo4j.driver(
+            process.env.NEO4J_URL,
+            neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD),
+        );
+    } catch (err) {
+        console.error(`Error while connecting to the neo4j database`);
+        console.error(err);
+        process.exit(-1);
+    }
 }
