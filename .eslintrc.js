5cd26445631ee694559708cda861ce0f39068d0a
Jason Athanasoglou <jathanasoglou@outlook.com>
2020-07-10 14:33:50 +0300

    replace tslint with eslint, add prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['prettier', '@typescript-eslint'],
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    rules: {
+        '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
+        semi: ['error', 'always'],
+    },
+};
