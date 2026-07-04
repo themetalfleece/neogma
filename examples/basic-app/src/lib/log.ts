c8e540cc025999fe5276dfd2d8fdb6a7e46b257e
Jason Athanasoglou <jathanasoglou@outlook.com>
2026-07-04 13:58:45 +0300

    Delete basic-app

--- examples/basic-app/src/lib/log.ts
+++
@@ -1,7 +0,0 @@
-export const section = (title: string): void => {
-  console.log(`\n──── ${title} ────`);
-};
-
-export const log = (...args: unknown[]): void => {
-  console.log('  ', ...args);
-};
