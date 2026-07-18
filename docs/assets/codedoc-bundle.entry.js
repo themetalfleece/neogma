51c71383a0a2918f5613197fc576a812823bcea7
Jason Athanasoglou <jathanasoglou@outlook.com>
2026-07-19 00:22:44 +0300

    Build

--- docs/assets/codedoc-bundle.entry.js
+++ docs/assets/codedoc-bundle.entry.js
@@ -1,35 +1,35 @@
-import { getRenderer } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/transport/renderer.js';
-import { initJssCs } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/transport/setup-jss.js';initJssCs();
-import { installTheme } from '/home/runner/work/neogma/neogma/documentation/.codedoc/content/theme.ts';installTheme();
-import { codeSelection } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/selection.js';codeSelection();
-import { sameLineLengthInCodes } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/same-line-length.js';sameLineLengthInCodes();
-import { initHintBox } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-hint/index.js';initHintBox();
-import { initCodeLineRef } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-ref/index.js';initCodeLineRef();
-import { initSmartCopy } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/smart-copy.js';initSmartCopy();
-import { copyHeadings } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/heading/copy-headings.js';copyHeadings();
-import { contentNavHighlight } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/contentnav/highlight.js';contentNavHighlight();
-import { loadDeferredIFrames } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/transport/deferred-iframe.js';loadDeferredIFrames();
-import { smoothLoading } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/transport/smooth-loading.js';smoothLoading();
-import { tocHighlight } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
-import { postNavSearch } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
-import { copyLineLinks } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-links/copy-line-link.js';copyLineLinks();
-import { gatherFootnotes } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/footnote/gather-footnotes.js';gatherFootnotes();
-import { ToCPrevNext } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
-import { CollapseControl } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/collapse/collapse-control.js';
-import { GithubSearch } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/misc/github/search.js';
-import { ToCToggle } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
-import { DarkModeSwitch } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
-import { ConfigTransport } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
-import { TabSelector } from '/home/runner/work/neogma/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es6/components/tabs/selector.js';
+import { getRenderer } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/transport/renderer.js';
+import { initJssCs } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/transport/setup-jss.js';initJssCs();
+import { installTheme } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/content/theme.ts';installTheme();
+import { codeSelection } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/code/selection.js';codeSelection();
+import { sameLineLengthInCodes } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/code/same-line-length.js';sameLineLengthInCodes();
+import { initHintBox } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/code/line-hint/index.js';initHintBox();
+import { initCodeLineRef } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/code/line-ref/index.js';initCodeLineRef();
+import { initSmartCopy } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/code/smart-copy.js';initSmartCopy();
+import { copyHeadings } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/heading/copy-headings.js';copyHeadings();
+import { contentNavHighlight } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/page/contentnav/highlight.js';contentNavHighlight();
+import { loadDeferredIFrames } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/transport/deferred-iframe.js';loadDeferredIFrames();
+import { smoothLoading } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/transport/smooth-loading.js';smoothLoading();
+import { tocHighlight } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/page/toc/toc-highlight.js';tocHighlight();
+import { postNavSearch } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/page/toc/search/post-nav/index.js';postNavSearch();
+import { copyLineLinks } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/code/line-links/copy-line-link.js';copyLineLinks();
+import { gatherFootnotes } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/footnote/gather-footnotes.js';gatherFootnotes();
+import { ToCPrevNext } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/page/toc/prevnext/index.js';
+import { CollapseControl } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/collapse/collapse-control.js';
+import { GithubSearch } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/misc/github/search.js';
+import { ToCToggle } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/page/toc/toggle/index.js';
+import { DarkModeSwitch } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/darkmode/index.js';
+import { ConfigTransport } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/transport/config.js';
+import { TabSelector } from 'C:/Users/jatha/Dev/neogma-doc/neogma/documentation/.codedoc/node_modules/@codedoc/core/dist/es5/components/tabs/selector.js';
 
 const components = {
-  'AsY3Cz3g0O4EhZ4OzvMrWQ==': ToCPrevNext,
-  'KX5ZK67OHCU+/01r/gHlLQ==': CollapseControl,
-  'P326USDq1PcQ0EFNW+ygZA==': GithubSearch,
-  'uEyFByOUMxM0e17JubQPxA==': ToCToggle,
-  'YYrmLrayDkYmTa1Ak0jt2w==': DarkModeSwitch,
-  'lccoUbeCvlKlHHNP78vUCg==': ConfigTransport,
-  '6u5c/vYs/0UUli2bssj/jA==': TabSelector
+  '1VYSfG1NOtFnNfpZYyXT3Q==': ToCPrevNext,
+  'rZ6GD2xZ7Y/6jHZFQVwyHQ==': CollapseControl,
+  'Z1/BjHkYEBgiPvPHvCo5LA==': GithubSearch,
+  'pNQ45pxBuR50ci/77jtkyg==': ToCToggle,
+  'BP7SoJ38K2MfOSaP/VtgTQ==': DarkModeSwitch,
+  'o8YL/Eofio1h7Dqa1zYYCQ==': ConfigTransport,
+  'fMnn5UcrAlFwT8hu+Sta/Q==': TabSelector
 };
 
 const renderer = getRenderer();
